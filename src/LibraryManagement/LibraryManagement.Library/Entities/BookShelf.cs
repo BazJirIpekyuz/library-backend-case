c2ac59bb111ecf167dad1f7092ffe2ffe7e03c2f
Baz Jir Ipekyuz <ipekyuz.bazjir@gmail.com>
2023-05-22 17:43:12 +0300

    Exercise-1.3-organize-data-in-collections-and-establish-test-cases: Refactoring

--- src/LibraryManagement/LibraryManagement.Library/Entities/BookShelf.cs
+++ src/LibraryManagement/LibraryManagement.Library/Entities/BookShelf.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public int RowId { get; set; }
         public string BookShelfNumber { get; set; }
-        public List<Book> Books { get; set; } = new List<Book>();
+        internal List<Book> Books { get; set; } = new List<Book>();
     }
 }
--- src/LibraryManagement/LibraryManagement.Library/Models/BookDto.cs
+++ src/LibraryManagement/LibraryManagement.Library/Models/BookDto.cs
@@ -8,7 +8,7 @@ namespace LibraryManagement.Library.Models
 {
     public class BookDto: LibraryItemDto
     {
-        public List<string> Authors { get; set; } = new List<string>();
+        public List<string> Authors { get; set; }
         public string Publisher { get; set; }
         public int PublicationYear { get; set; }
         public int NumberOfPages { get; set; }
